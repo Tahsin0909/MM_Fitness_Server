08f93dee9337e21689a90f40d51ffbf1caf83b84
Tahsin0909 <tajisan789@gmail.com>
2023-11-30 21:52:54 +0600

    Added Jwt Token And Verified

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken')
 const port = process.env.PORT || 5000;
 require('dotenv').config()
 const app = express()
@@ -35,6 +36,30 @@ async function run() {
         const ForumsCollection = database.collection('Forums')
         const ClassCollection = database.collection('Class')
 
+
+        // jwt related api
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: '1h' });
+            res.send({ token });
+        })
+
+            // middlewares 
+    const verifyToken = (req, res, next) => {
+        console.log('inside verify token', req.headers.authorization);
+        if (!req.headers.authorization) {
+          return res.status(401).send({ message: 'unauthorized access' });
+        }
+        const token = req.headers.authorization.split(' ')[1];
+        jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+          if (err) {
+            return res.status(401).send({ message: 'unauthorized access' })
+          }
+          req.decoded = decoded;
+          next();
+        })
+      }
+
         //User 
         app.post('/users', async (req, res) => {
             const user = req.body;
@@ -42,6 +67,22 @@ async function run() {
             const result = await UserCollection.insertOne(user)
             res.send(result)
         })
+        app.get('/users/admin/:email', verifyToken, async (req, res) => {
+            const email = req.params.email;
+      
+            if (email !== req.decoded.email) {
+              return res.status(403).send({ message: 'forbidden access' })
+            }
+      
+            const query = { email: email };
+            const user = await UserCollection.findOne(query);
+            let admin = false;
+            if (user) {
+              admin = user?.role === 'trainer';
+            }
+            res.send({ admin });
+          })
+
         app.get('/users', async (req, res) => {
             const cursor = UserCollection.find()
             const result = await cursor.toArray()
@@ -142,7 +183,7 @@ async function run() {
                 res.send(result)
             }
         })
-        app.get('/subscriber', async (req, res) => {
+        app.get('/subscriber',verifyToken, async (req, res) => {
             const cursor = SubscriberCollection.find()
             const result = await cursor.toArray()
             res.send(result)
@@ -261,7 +302,7 @@ async function run() {
         })
         app.get('/class/:email', async (req, res) => {
             const email = req.params.email
-            const query = {trainerEmail: email}
+            const query = { trainerEmail: email }
             const cursor = ClassCollection.find(query)
             const result = await cursor.toArray()
             res.send(result)
